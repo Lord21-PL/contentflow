8cfdcb92e92d5de0c3dc0d5d82b3487bf0757d1b
Lord21 <patriko9999@gmail.com>
2025-07-06 18:17:50 +0200

    ContentFlowAI 0.91

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || '/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    // USUNĘLIŚMY BŁĘDNĄ, GLOBALNĄ DEFINICJĘ NAGŁÓWKA 'Content-Type'
 });
 
 // Axios interceptor to add the token to every request
